e70c4506448e52c253ad4c05650c99077ddb0ba2
jwzimmer1990 <jwzimmer1990@gmail.com>
2016-02-19 17:35:26 +0200

    refactoring kaprekar_routine
    took out repetitive code chunks and made them helper functions instead

--- kaprekarconstant6174.py
+++ kaprekarconstant6174.py
@@ -8,6 +8,24 @@
 
 #followed PEP8 more in this file
 
+#helper function for kaprekar_routine function
+def add_0s_front_of_n(stringn, desirednlength):
+    """takes string of n, desired length of n, puts 0s in front of n
+    so that n will be desired length but will not change its value as an int"""
+    num0 = desirednlength - len(stringn)
+    str0 = "0"*num0
+    stringn = str0 + stringn
+    return stringn
+
+#helper function for kaprekar_routine function
+def make_int_from_list(numberlist):
+    """takes a list of digits and returns a number composed of the elements of the list"""
+    newint = ""
+    for x in numberlist:
+        newint += str(x)
+    newint = int(newint)
+    return newint
+
 def kaprekar_routine(n, iterations=0):
     """Takes a positive integer n and returns how many iterations
     it took to get to 6174 (Kaprekar's constant)
@@ -17,9 +35,7 @@ def kaprekar_routine(n, iterations=0):
     else:
         strn = str(n)
         if len(strn) < 4:
-            num0 = 4 - len(strn)
-            str0 = "0"*num0
-            strn = str0 + strn
+            strn = add_0s_front_of_n(strn, 4)
         else:
             pass
         nlist = [int(x) for x in strn]
@@ -27,14 +43,8 @@ def kaprekar_routine(n, iterations=0):
         nlist.sort()
         #from large to small digits
         reversedlist = list(reversed(nlist))
-        largen = ""
-        for x in reversedlist:
-            largen += str(x)
-        largen = int(largen)
-        smalln = ""
-        for x in nlist:
-            smalln += str(x)
-        smalln = int(smalln)
+        largen = make_int_from_list(reversedlist)
+        smalln = make_int_from_list(nlist)
         newn = largen - smalln
         iterations += 1
         #print n, newn, largen, smalln, iterations
@@ -51,33 +61,11 @@ def kaprekar_routine_negative(n, iterations=0):
     #i'll need to cut off from doing infinite recursions, since it's possible there won't be any equivalent
     #to kaprekar's constant, and if there are no loops and no constant, then the function would recurse forever
     #SOME PSEUDOCODE & COPYPASTA:
-    if n == -6174 or n is in a loop:
-        return iterations
-    else:
-        strn = str(n)
-        if len(strn) < 4:
-            num0 = 4 - len(strn)
-            str0 = "0"*num0
-            strn = str0 + strn
-        else:
-            pass
-        nlist = [int(x) for x in strn]
-        #from small to large digits
-        nlist.sort()
-        #from large to small digits
-        reversedlist = list(reversed(nlist))
-        largen = ""
-        for x in reversedlist:
-            largen += str(x)
-        largen = int(largen)
-        smalln = ""
-        for x in nlist:
-            smalln += str(x)
-        smalln = int(smalln)
-        newn = largen - smalln
-        iterations += 1
-        #print n, newn, largen, smalln, iterations
-        return kaprekar_routine(newn, iterations)
+    #if n == -6174: # or n is in a loop:
+        #return iterations
+    #else:
+        #stuff
+    return None
 
 def main():
     print kaprekar_routine(124)
