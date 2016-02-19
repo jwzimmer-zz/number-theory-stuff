376be1b2792bd9a53c7ee569e93556354d3ea49b
jwzimmer1990 <jwzimmer1990@gmail.com>
2016-02-19 16:35:16 +0200

    added kaprekar constant file

---
+++ kaprekarconstant6174.py
@@ -0,0 +1,8 @@
+#kaprekar constant 6174 kaprekar routine
+
+#followed PEP8 more in this file
+
+def kaprekar_routine(n):
+    """Takes a positive integer n and returns how many iterations
+    it took to get to 6174 (Kaprekar's constant)"""
+    
--- superperfect.py
+++ superperfect.py
@@ -1,4 +1,4 @@
-#kaprekar number 6147
+
 #3435 digit to digit invariant
 #Note the lists of prime factorizations for composite numbers came from http://www.naturalnumbers.org/composites.html
 #6 is a perfect number which is a sum of prime factors and 1, it's probably unique because most numbers are composed of #relatively smaller prime factors so the sum and the number diverges, what is the pattern of the differences?
