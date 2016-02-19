ade9e121cc73837cfe7bb96dd581dc03a63da5c7
jwzimmer1990 <jwzimmer1990@gmail.com>
2016-02-19 17:11:49 +0200

    kaprekar routine for negative numbers
    added thoughts and pseudocode, want to look for a loop or constant equivalent to kaprekar constant for negative 4 digit numbers
    
    maybe later - lengths of digits other than 4?

--- kaprekarconstant6174.py
+++ kaprekarconstant6174.py
@@ -40,6 +40,45 @@ def kaprekar_routine(n, iterations=0):
         #print n, newn, largen, smalln, iterations
         return kaprekar_routine(newn, iterations)
 
+#what happens if you use negative numbers? do you loop at 6174 still? How would that work?
+def kaprekar_routine_negative(n, iterations=0):
+    """Takes a negative integer n and looks for a loop,
+    or returns how many iterations
+    it took to get to (-)6174 (Kaprekar's constant)
+    Input should be 4 or fewer digits and not a repdigit"""
+    #i think i'll need to ADD numbers not subtract them since we want the length of the number to stay 4 or under
+    #i'll need to store previous numbers in order to recognize repeating loops
+    #i'll need to cut off from doing infinite recursions, since it's possible there won't be any equivalent
+    #to kaprekar's constant, and if there are no loops and no constant, then the function would recurse forever
+    #SOME PSEUDOCODE & COPYPASTA:
+    if n == -6174 or n is in a loop:
+        return iterations
+    else:
+        strn = str(n)
+        if len(strn) < 4:
+            num0 = 4 - len(strn)
+            str0 = "0"*num0
+            strn = str0 + strn
+        else:
+            pass
+        nlist = [int(x) for x in strn]
+        #from small to large digits
+        nlist.sort()
+        #from large to small digits
+        reversedlist = list(reversed(nlist))
+        largen = ""
+        for x in reversedlist:
+            largen += str(x)
+        largen = int(largen)
+        smalln = ""
+        for x in nlist:
+            smalln += str(x)
+        smalln = int(smalln)
+        newn = largen - smalln
+        iterations += 1
+        #print n, newn, largen, smalln, iterations
+        return kaprekar_routine(newn, iterations)
+
 def main():
     print kaprekar_routine(124)
     
