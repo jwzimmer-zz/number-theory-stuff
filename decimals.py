6defc1f85311017e09d9b552d6b23b64f217e4fe
jwzimmer1990 <jwzimmer1990@gmail.com>
2016-02-23 00:27:02 +0200

    thoughts on decimals

---
+++ decimals.py
@@ -0,0 +1,13 @@
+# 1/998000.0 seems to give you powers of two in 3 digits
+# 001, 002, 004, 008, 016, 032, 064, etc. when it gets longer than 3 digits, 513 -> 1026, it shows only the last 3
+# as in 513026. According to wolfram alpha the period is 498
+# https://www.wolframalpha.com/input/?i=1%2F99800
+# 
+# 1/998002 doesn't show an obvious pattern
+# neither does 1/997999
+# although wolfram alpha shows it as having a large period
+# 1/998001 shows all the 3 digit numbers except 998
+# 0.001002003004...
+# https://www.youtube.com/watch?v=daro6K6mym8
+# 
+# 
