8aa0b0ab5a2c3930788b4a35d98e54e119dcef6c
jwzimmer1990 <jwzimmer1990@gmail.com>
2016-02-23 04:37:01 +0200

    pronic number?

---
+++ fortytwo.py
@@ -0,0 +1,5 @@
+#primary pseudoperfect numbers
+#pronic number
+#is there a pronic number whose prime factorization is also the product of consecutive integers? (and whose
+#prime factorization is not its only factorization)
+#42 = 6 * 7 but it's prime factorization is 2 * 3 * 7.
