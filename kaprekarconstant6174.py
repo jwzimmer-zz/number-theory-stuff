d99016b48963ea74ffe56ed23e9043999482f321
jwzimmer1990 <jwzimmer1990@gmail.com>
2016-02-25 17:26:23 +0200

    clarified comments

--- kaprekarconstant6174.py
+++ kaprekarconstant6174.py
@@ -50,13 +50,12 @@ def kaprekar_routine(n, iterations=0):
         #print n, newn, largen, smalln, iterations
         return kaprekar_routine(newn, iterations)
 
-#what happens if you use negative numbers? do you loop at 6174 still? How would that work?
+#what happens if you use negative numbers? do you loop at (-)6174 still? How would that work?
 def kaprekar_routine_negative(n, iterations=0):
     """Takes a negative integer n and looks for a loop,
     or returns how many iterations
     it took to get to (-)6174 (Kaprekar's constant)
     Input should be 4 or fewer digits and not a repdigit"""
-    #i think i'll need to ADD numbers not subtract them since we want the length of the number to stay 4 or under
     #i'll need to store previous numbers in order to recognize repeating loops
     #i'll need to cut off from doing infinite recursions, since it's possible there won't be any equivalent
     #to kaprekar's constant, and if there are no loops and no constant, then the function would recurse forever
